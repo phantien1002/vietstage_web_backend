878d0cc7d0b77f3190805f8d2282deb57b131fe0
ThanhTaiNguyen04 <taintse183488@fpt.edu.vn>
2026-07-28 23:11:13 +0700

    refactor: replace SMTP with SendGrid REST API

--- src/main/java/com/example/vietstage_web_be/service/IEmailService.java
+++ src/main/java/com/example/vietstage_web_be/service/IEmailService.java
@@ -1,7 +1,5 @@
 package com.example.vietstage_web_be.service;
 
-import jakarta.mail.MessagingException;
-
 public interface IEmailService {
-    void sendOtpEmail(String to, String otpCode, String subject, String messageTemplate) throws MessagingException;
+    void sendOtpEmail(String to, String otpCode, String subject, String messageTemplate) throws Exception;
 }
